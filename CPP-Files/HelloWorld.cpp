ad325829d9866259404984dfbef91929984621d7
tangdihao <3430973359@qq.com>
2019-02-07 17:08:00 +0800

    Update HelloWorld.cpp

--- CPP-Files/HelloWorld.cpp
+++ CPP-Files/HelloWorld.cpp
@@ -1,8 +1,15 @@
-#include<iostream>
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : tangdihao
+// Version     :
+// Copyright   : Bilibili UID 148000902
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
 using namespace std;
 
-int main()
-{
-	cout<<"HelloWorld"<<endl;
+int main() {
+	cout << "Hello World" << endl; // prints Hello World
 	return 0;
 }
