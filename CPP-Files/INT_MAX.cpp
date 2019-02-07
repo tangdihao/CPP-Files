5429c747be0f062612a6e48cea0617f83a39e7a5
tangdihao <3430973359@qq.com>
2019-02-07 21:54:37 +0800

    Create INT_MAX.cpp

---
+++ CPP-Files/INT_MAX.cpp
@@ -0,0 +1,17 @@
+/**
+ * @name    INT_MAX.cpp
+ * @author  tangdihao
+ * @version 1.0.0
+ * --------------------------------
+ * Copyright (c) 2019 by tangdihao.
+ * All Rights Reserved.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{   //Print INT type maximum
+    cout<< INT_MAX <<endl;
+    return 0;
+}
