15d670c0eea52c9f8a5668b0ef9ae8f4e729f5c2
tangdihao <3430973359@qq.com>
2019-02-07 21:54:41 +0800

    Create INT_MIN.cpp

---
+++ CPP-Files/INT_MIN.cpp
@@ -0,0 +1,17 @@
+/**
+ * @name    INT_MIN.cpp
+ * @author  tangdihao
+ * @version 1.0.0
+ * --------------------------------
+ * Copyright (c) 2019 by tangdihao.
+ * All Rights Reserved.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{   //Print INT type minimum
+    cout<< INT_MIN <<endl;
+    return 0;
+}
