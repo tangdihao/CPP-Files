e72300533c9803949cc1516382be1b39bc41edac
tangdihao <3430973359@qq.com>
2019-02-08 13:22:11 +0800

    Update Add of 3 ints.cpp

--- CPP-Files/Add of 3 ints.cpp
+++ CPP-Files/Add of 3 ints.cpp
@@ -3,6 +3,8 @@
  * @author  tangdihao
  * @version 1.0.0
  * --------------------------------
+ * Add three numbers
+ * --------------------------------
  * Copyright (c) 2019 by tangdihao.
  * All Rights Reserved.
  */
