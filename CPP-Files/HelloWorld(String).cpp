f4b820a12e7a6d903779f3ca6be939247f690caf
tangdihao <3430973359@qq.com>
2019-02-08 11:42:07 +0800

    Create HelloWorld(String).cpp

---
+++ CPP-Files/HelloWorld(String).cpp
@@ -0,0 +1,19 @@
+/**
+ * @name    HelloWorld(String).cpp
+ * @author  tangdihao
+ * @version 1.0.0
+ * --------------------------------
+ * Copyright (c) 2019 by tangdihao.
+ * All Rights Reserved.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    typedef string sentence;
+    sentence say_hello = "Hello World";//将say_hello定义为HelloWorld
+    cout<< say_hello <<endl;//输出say_hello
+    return 0;
+}
